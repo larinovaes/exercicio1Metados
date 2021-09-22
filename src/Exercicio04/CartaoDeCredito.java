3c96b56bf2b01cf6fa74603d4e23b96658fbb547
Larissa Novais <larinovais54@gmail.com>
2021-09-21 21:48:15 -0300

    resolvendo exercicio 04 cartão de credito

--- src/Exercicio04/CartaoDeCredito.java
+++ src/Exercicio04/CartaoDeCredito.java
@@ -1,60 +1,65 @@
 package Exercicio04;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 public class CartaoDeCredito {
     Scanner leitor = new Scanner(System.in);
-    double limite = 1000;
+
+    double limiteTotal;
+    double limiteAtual;
     double saldo;
     double fatura;
     String nomeDoCliente;
 
     //metado construtor
-    public CartaoDeCredito(String nomeDoCliente, double limite, double saldo, double fatura) {
+    public CartaoDeCredito(String nomeDoCliente, double limiteTotal, double saldo, double fatura) {
         this.fatura = fatura;
-        this.limite = limite;
+        this.limiteTotal = limiteTotal;
+        this.limiteAtual = limiteTotal;
         this.saldo = saldo;
         this.nomeDoCliente = nomeDoCliente;
     }
 
-    //Metado mostrar dadoss
+    //Metado mostrar dados
     public void mostrarDados() {
         System.out.println("Nome do cliente: " + nomeDoCliente);
-        System.out.println("Limite: R$" + limite);
+        System.out.println("Limite: R$" + limiteAtual);
         System.out.println("Saldo de compras: " + saldo);
     }
 
     // Metado para relaziar compra no cartão
     public void realizarCompra(double fazendoCompras) {
-        double passandoDoLimite = limite - fazendoCompras;
 
-        if (limite > fazendoCompras) {
-            System.out.println("Compra relalizada com sucesso!");
-            System.out.println("Seu saldo para compras é: " + passandoDoLimite);
-        } else if (fazendoCompras > limite) {
+        if (limiteAtual >= fazendoCompras) {
+            limiteAtual -= fazendoCompras;
+            System.out.println("Você realizou uma compra no valor de: R$" + fazendoCompras);
+            System.out.println("Limite atual é: R$" + limiteAtual);
+        } else {
             System.out.println("Você atigiu o seu limite, a compra não pode ser efetuada!");
         }
     }
 
-    public void imprimirFatua() {
-        System.out.println("\nValor a ser pago será: R$" + fatura);
-
+    public void imprimirFatura() {
+        double valorFatura = limiteTotal - limiteAtual;
+        System.out.println("\nValor a ser pago será: R$" + valorFatura);
     }
 
-    public void aumentarLimite(boolean menu) {
-        while (menu) {
-            System.out.println("Se deseja aumentar seu limite digite 1");
-            System.out.println("Se deseja sair deigite 2");
-            int opcao = leitor.nextInt();
-            leitor.nextLine();
-            if (opcao == 1) {
-                System.out.println("Seu limite foi liberado para aumento!");
-                System.out.println("Seu limite atual é: R$" + (limite * 2));
-            } else if (opcao == 2) {
-                System.out.println("Voce saiu do programa!");
-                menu = false;
-            }
+    public void aumentarLimite() {
+        System.out.println("Se deseja aumentar seu limite digite 1");
+        System.out.println("Se deseja sair deigite 2");
+        int opcao = leitor.nextInt();
+        leitor.nextLine();
+
+        if (opcao == 1) {
+            limiteAtual += limiteTotal;
+            limiteTotal *= 2;
+            System.out.println("Seu limite foi liberado para aumento!");
+            System.out.println("Seu limite atual é: R$" + limiteAtual);
+        } else if (opcao == 2) {
+            System.out.println("Voce saiu do programa!");
+        } else {
+            System.out.println("Por favor digite uma opção válida!!");
         }
     }
 }
+
--- src/Exercicio04/Main.java
+++ src/Exercicio04/Main.java
@@ -1,6 +1,12 @@
 package Exercicio04;
 
-import java.util.Scanner;
+/**
+Crie uma classe que represente um cartão de crédito, que deve ter como
+atributos o seu limite, o saldo da fatura e nome do cliente. Crie também os métodos
+aumentaLimite(), diminuiLimite(), realizaCompra() que não deve permitir uma
+compra caso o limite tenha sido atingido e o imprimeFatura() que imprime na tela o
+valor da fatura.
+ */
 
 public class Main {
 
@@ -9,15 +15,18 @@ public class Main {
         CartaoDeCredito dados = new CartaoDeCredito("Larissa Novais", 1000,0,0);
         dados.mostrarDados();
         System.out.println("-----------------------------");
-        dados.realizarCompra(100);
-        dados.realizarCompra(400);
-        dados.realizarCompra(2000);
+        dados.realizarCompra(500);
+        dados.realizarCompra(140);
+        dados.realizarCompra(500);
+
         System.out.println("----Fatura mensal-----");
         System.out.println("\nNome do titular do cartão: " + (dados.nomeDoCliente));
-        System.out.println("\nLimite disponivel: R$" +(dados.limite));
+        System.out.println("\nLimite disponivel: R$" +(dados.limiteAtual));
+        dados.imprimirFatura();
         System.out.println("-----------------------------------------");
-        dados.aumentarLimite(true);
+        dados.aumentarLimite();
         dados.mostrarDados();
+        System.out.println("------------------------------------------");
 
 
 
