e069488656fa492e9ab0de1cd6109d0ec91868f4
Larissa Novais <larinovais54@gmail.com>
2021-09-21 22:32:35 -0300

    treinando uso do get e set

--- src/ListaDeExerciciosComplementar/Bola_Exercicio01.java
+++ src/ListaDeExerciciosComplementar/Bola_Exercicio01.java
@@ -6,10 +6,10 @@ package ListaDeExerciciosComplementar;
  * 2. Métodos: trocaCor e mostraCor
  */
 public class Bola_Exercicio01 {
-    String nome;
-    String cor;
-    String circunferencia;
-    String material;
+    private String nome;
+    private String cor;
+    private String circunferencia;
+    private String material;
 
     public Bola_Exercicio01(String nome,String cor,String circunferencia,String material){
         this.circunferencia = circunferencia;
@@ -17,12 +17,21 @@ public class Bola_Exercicio01 {
         this.material = material;
         this.nome = nome;
     }
-    public void trocarCor(String trocarAcor){
-
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome1){
+        nome = nome1;
+    }
+    public String getCor(){
+        return cor;
+    }
+    public void setCor(String mudarAcor){
+        cor = mudarAcor;
     }
     public void mostrarDados(){
         System.out.println("Nome da Bola: " + nome);
-        System.out.println("Cor De referencia: " + cor);
+        System.out.println("Cor: " + cor);
         System.out.println("Circunferência: " + circunferencia);
         System.out.println("Material: " + material);
     }
--- src/ListaDeExerciciosComplementar/Main.java
+++ src/ListaDeExerciciosComplementar/Main.java
@@ -1,11 +1,24 @@
 package ListaDeExerciciosComplementar;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Bola_Exercicio01 dados = new Bola_Exercicio01("Jabulani","azul","Redonda","Plastico");
+        Bola_Exercicio01 dados = new Bola_Exercicio01("Jabulani", "azul", "Redonda", "Plastico");
         dados.mostrarDados();
         System.out.println("---------------------");
-        dados.trocarCor("Verde");
-        dados.mostrarDados();
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual nome deseja colocar na bola?");
+        String nome = leitor.nextLine();
+
+        dados.setNome(nome);
+        System.out.println("Nome da bola atual" + dados.getNome());
+
+        System.out.println("Qual cor deseja para sua bola?");
+        String mudarCor = leitor.nextLine();
+
+        dados.setCor(mudarCor);
+        System.out.println("Cor atual da bola: " + dados.getCor());
     }
 }
