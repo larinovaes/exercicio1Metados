20145de26a3ebfc647b3f55201c806b036e3027a
Larissa Novais <larinovais54@gmail.com>
2021-09-28 09:35:12 -0300

    fazendo ajustes

--- src/ExercicioImobiliaria/Imobiliaria.java
+++ src/ExercicioImobiliaria/Imobiliaria.java
@@ -1,18 +1,26 @@
 package ExercicioImobiliaria;
 
 
+import java.util.List;
+
 public class Imobiliaria {
 
     Imovel imoveis;
 
-    public Imobiliaria( Imovel imoveis){
+    public Imobiliaria(Imovel imoveis){
         this.imoveis = imoveis;
+    }
+
+    public void exibirListaDeImoveis(){
+        for (Imovel imoveis: imoveis.getImovels()) {
+            System.out.println(imoveis);
+        }
     }
 
     @Override
     public String toString() {
         StringBuilder listaImovel = new StringBuilder();
-       listaImovel.append("Imovel: "+ imoveis);
+       listaImovel.append("---- Imovel ----- "+ imoveis);
 
        return listaImovel.toString();
     }
--- src/ExercicioImobiliaria/Main.java
+++ src/ExercicioImobiliaria/Main.java
@@ -10,7 +10,7 @@ public class Main {
         inquilino1.adicionandoMorador(inquilino1);
         imovel1.addImovels(imovel1);
        // imovel1.exibirImovel();
-        // System.out.println("---Moradores---- "+ "\n"+ inquilino1);
+        //System.out.println("---Moradores---- "+ "\n"+ inquilino1);
 
         Imovel imovel2 = new Imovel("R. larissa","Joao","CASA AMARELA",2344);
         Moradores inquilino3 = new Moradores("Pedro","2733-2352");
@@ -24,11 +24,11 @@ public class Main {
         inquilino3.adicionandoMorador(inquilino5);
         imovel3.addImovels(imovel3);
         //imovel3.exibirImovel();
-        //System.out.println("---Moradores---- "+ "\n"+ inquilino5);
 
-        Imobiliaria imobiliaria = new Imobiliaria(imovel1);
-        System.out.println(imobiliaria);
 
+        Imobiliaria imobiliaria = new Imobiliaria(imovel3);
+        imobiliaria.exibirListaDeImoveis();
+        System.out.println("---Moradores---- "+ "\n"+ inquilino5);
 
 
 
